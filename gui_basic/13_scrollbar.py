fd66c833e28f04f8570a3333b53568238783aa0d
yoonkeen <agy91521947@gmail.com>
2024-11-25 00:34:19 +0900

    gui_13_scrollbar

---
+++ gui_basic/13_scrollbar.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+root.title("Scroll Bar")
+root.geometry("640x480")
+
+frame = Frame(root)
+frame.pack()
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side = "right", fill="y")
+
+listbox = Listbox(frame, selectmode = "extended", height = 10, yscrollcommand=scrollbar.set)
+for i in range(1,32):
+    listbox.insert(END, "day"+str(i))
+listbox.pack(side = "left")
+
+scrollbar.config(command=listbox.yview)
+
+root.mainloop()
